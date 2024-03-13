6722c964a1796122d70e028b672922e200de2b24
turtle-tim <149429545+turtle-tim@users.noreply.github.com>
2024-03-13 12:05:51 -0500

    Update db.js
    instead of URL, take URI

--- src/db/db.js
+++ src/db/db.js
@@ -2,16 +2,13 @@ const mongoose = require("mongoose");
 
 const { logger } = require("../utils/logger/loggerUtils");
 
-mongoose
-  .connect(process.env.MONGODB_URL, {
+async()=>{
+  mongoose. connect(
+    await process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: true,
     useUnifiedTopology: true,
-  })
-  .then(() => {
-    logger.info("Connected to database");
-  })
-  .catch((error) => {
-    logger.error("Failed to connect to database!!!", error);
-  });
+  }.then(logger.info("Connected to db")).catch(err){console.log(err)}
+  )
+}
